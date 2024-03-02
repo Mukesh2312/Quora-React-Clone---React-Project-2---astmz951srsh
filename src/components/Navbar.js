6d12ac78aa0d549c70210109fc096602a0d91225
Mukesh2312 <49585682+Mukesh2312@users.noreply.github.com>
2024-03-02 16:58:41 +0530

    code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import '../styles/Navbar.css'
 import Logo from './Logo'
+import FollowingIcon from './FollowingIcon'
+import HomeIcon from './HomeIcon'
+import ComposeIcon from './ComposeIcon'
+import GroupIcon from './GroupIcon'
+import Notification_icon from './Notification_icon'
+import { FaSearch } from "react-icons/fa"
 
 function Navbar() {
 
@@ -12,17 +18,46 @@ function Navbar() {
 
                     <Logo />
                 </div>
-                <button>Add Quetion</button>
+                <div className="header_navigation_links">
+
+
+                    <div className="home_icon_container header_icons">
+                        <HomeIcon />
+                    </div>
+                    <div className="following_icon_container header_icons">
+                        <FollowingIcon />
+                    </div>
+                    <div className="compose_icon header_icons">
+                        <ComposeIcon />
+                    </div>
+                    <div className="group_icon header_icons">
+                        <GroupIcon />
+                    </div>
+                    <div className="notification_icon header_icons">
+                        <Notification_icon />
+                    </div>
+
+                    <div className="search_box">
+                        <input type="search" placeholder=' Search Qoura' />
+
+
+                    </div>
+                </div>
+
                 <div className="register">
                     <NavLink to='/register'>Register</NavLink>
                     <NavLink to='/Login'>Login</NavLink>
+                </div>
+                <div className="add_question">
+
+                    <button>Add Quetion</button>
                 </div>
             </div>
 
 
 
 
-        </div>
+        </div >
     )
 }
 
