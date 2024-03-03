2f7eff5d457a536503f4ec40a4ac7ad93ef12306
Mukesh2312 <49585682+Mukesh2312@users.noreply.github.com>
2024-03-03 10:10:17 +0530

    code

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import '../styles/Post.css';
+import PostCard from './PostCard';
 
 function Post() {
     const [post, setPost] = useState([]);
@@ -20,45 +21,7 @@ function Post() {
         <div id='Post_section'>
             <h2>Post</h2>
             <div className="post_content_container">
-                {
-                    post.map((pst, index) => {
-                        return (
-                            <div className="auther_details_wrapper" key={index}>
-                                <div className="auther_personal_details">
-
-                                    <div className="auther_img">
-                                        <img src={pst.author.profileImage} alt="" />
-                                    </div>
-                                    <div className="auther_name">
-                                        <small>{pst.author.name}</small>
-                                    </div>
-                                </div>
-                                <div className="author_post_content">
-
-                                    <div className="author_post_text">
-                                        <div className="author_post_text_heading">
-                                            <h4>
-                                                {pst.title}
-                                            </h4>
-                                        </div>
-                                        <div className="author_post_text_body">
-                                            <p>
-
-                                                {pst.content}
-                                            </p>
-
-                                        </div>
-                                    </div>
-                                    <div className="author_post_img">
-
-                                        <img src={pst.images[0]} alt="" />
-                                    </div>
-
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+                <PostCard post={post} />
             </div>
         </div>
     )
---
+++ src/components/PostCard.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Upvote from './Upvote';
+import Downvote from './Downvote';
+import CommentIcon from './CommentIcon';
+
+function PostCard(props) {
+    const { post } = props;
+
+    return (
+        <div id='post-card'>
+            {
+                post.map((pst, index) => {
+                    return (
+                        <div className="auther_details_wrapper" key={index}>
+                            <div className="auther_personal_details">
+
+                                <div className="auther_img">
+                                    <img src={pst.author.profileImage} alt="" />
+                                </div>
+                                <div className="auther_name">
+                                    <small>{pst.author.name}</small>
+                                </div>
+                            </div>
+                            <div className="author_post_content">
+
+                                <div className="author_post_text">
+                                    <div className="author_post_text_heading">
+                                        <h4>
+                                            {pst.title}
+                                        </h4>
+                                    </div>
+                                    <div className="author_post_text_body">
+                                        <p>
+
+                                            {pst.content}
+                                        </p>
+
+                                    </div>
+                                </div>
+                                <div className="author_post_img">
+
+                                    <img src={pst.images[0]} alt="" />
+                                </div>
+                                <div className="post_status_container">
+                                    <div className="post_vote_wrapper">
+                                        <div className="upvote">
+                                            <Upvote />
+
+                                        </div>
+                                        <div className="downvote">
+                                            <Downvote />
+                                        </div>
+                                    </div>
+                                    <div className="post_comment">
+                                        <CommentIcon />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default PostCard
